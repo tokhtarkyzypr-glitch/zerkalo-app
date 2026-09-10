66fdf47215de73517d7307d05e2d3829e2210468
tokhtarkyzypr-glitch <tokhtarkyzypr@gmail.com>
2026-09-10 13:45:13 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Zerkalo from "./App.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Zerkalo />
+  </React.StrictMode>
+);
